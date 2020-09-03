10b19dc9c0627a0e23e761e82e8952c08747160b
Hannah Read <hannahj.read13@gmail.com>
2020-09-02 19:54:30 -0500

    Studio unit testing complete

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -9,7 +10,48 @@ public class BalancedBracketsTest {
     //TODO: add tests here
     @Test
     public void emptyTest() {
+
         assertEquals(true, true);
+    }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void doubleBracketFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[["));
+    }
+
+    @Test
+    public void oneBracketFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void noBracketsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void sameNumberOfBracketsButNotSquaredFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]][["));
+    }
+
+    @Test
+    public void nestedBracketsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[]]"));
+    }
+
+    @Test
+    public void integersTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("1"));
+    }
+
+    @Test
+    public void curlyBracketsTrue () {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("{}"));
     }
 
 
